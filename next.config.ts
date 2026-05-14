e5000d3dd667dc703d1433319d15ec0d8a566ac1
burovgena-eng <burov.gena@gmail.com>
2026-05-14 20:40:41 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+    memoryUsageWorkers: 1,
+  },
 };
 
 export default nextConfig;
